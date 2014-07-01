64ae9943f5b5159e4912e2062753ce648f169c61
Nandita Rajagopalan <nandita@Nanditas-MacBook-Pro.local>
2014-07-01 11:08:15 -0500

    Removed query and used the available array for assignments.

--- studentwork/app/models/student.rb
+++ studentwork/app/models/student.rb
@@ -2,10 +2,9 @@ class Student < ActiveRecord::Base
   has_many :assignments
 
   def assignment_names
-    my_assignments = Assignment.where(student_id: self.id)
-    if (my_assignments.length > 0)
+    if (assignments.length > 0)
       "#{self.name} has the following assignments:" +
-       my_assignments.map(&:name).join(",")
+       assignments.map(&:name).join(",")
     else
       "#{self.name} has no assignments "
     end
